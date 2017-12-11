e4888588f158723c4877a04ce313323dc7101acc
Hassan Khan <hassan.khan@endemolshine.com>
2017-12-11 19:17:45 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# TSLint Config ESG
+
+[![NPM version][npm-image]][npm-url]
+[![NPM downloads][downloads-image]][downloads-url]
+[![Build status][travis-image]][travis-url]
+
+> A [TSLint config](https://palantir.github.io/tslint/usage/tslint-json/) for [Endemol Shine Group](http://endemolshinegroup.com/)
+
+## Installation
+
+```sh
+npm install tslint-config-esg --save
+```
+
+## Usage
+
+In `tslint.json`:
+
+```json
+{
+  "extends": "tslint-config-esg"
+}
+```
+
+**P.S.** Some TSLint rules may require the use of `--type-check` (and `--project`).
+
+### Rules
+
+* [TSLint](https://www.npmjs.com/package/tslint)
+* [TSLint ESLint Rules](https://www.npmjs.com/package/tslint-eslint-rules)
+
+### Help
+
+Many [**TSLint ESLint Rules**](https://github.com/buzinas/tslint-eslint-rules#rules-copied-from-the-eslint-website) are incomplete and can use your help!
+
+## License
+
+[npm-image]: https://img.shields.io/npm/v/tslint-config-esg.svg?style=flat
+[npm-url]: https://npmjs.org/package/tslint-config-esg
+[downloads-image]: https://img.shields.io/npm/dm/tslint-config-esg.svg?style=flat
+[downloads-url]: https://npmjs.org/package/tslint-config-esg
+[travis-image]: https://img.shields.io/travis/EndemolShineGroup/tslint-config-esg.svg?style=flat
+[travis-url]: https://travis-ci.org/EndemolShineGroup/tslint-config-esg
---
+++ tslint.js
@@ -0,0 +1,87 @@
+module.exports = {
+  extends: ['tslint-eslint-rules'],
+  rules: {
+
+    /**
+     * ES6
+     */
+    'ter-arrow-body-style'            : [true, 'always'],
+    'ter-arrow-parens'                : [true, 'always'],
+    'ter-arrow-spacing'               : true,
+    'no-var-keyword'                  : true,
+
+    /**
+     * Variables
+     */
+    'no-shadowed-variable'            : true,
+    'no-unused-variable'              : true,
+    'no-use-before-declare'           : true,
+
+    /**
+     * Possible errors
+     */
+    'trailing-comma'                  : [true, {'multiline': 'always'}],
+    'no-conditional-assignment'       : true,
+    'no-console'                      : true,
+    'no-debugger'                     : true,
+    'ban'                             : [true, ['alert']]
+    'no-constant-condition'           : true,
+    'no-duplicate-case'               : true,
+    'no-empty'                        : true,
+    'no-ex-assign'                    : true,
+    'no-extra-boolean-cast'           : true,
+    'no-extra-semi'                   : true,
+    'no-inner-declarations'           : true,
+    'no-invalid-regexp'               : true,
+    'ter-no-irregular-whitespace'     : true,
+    'ter-no-sparse-arrays'            : true,
+    'use-isnan'                       : true,
+
+    /**
+     * Best practices
+     */
+    'curly'                           : true,
+    'switch-default'                  : true,
+    'triple-equals'                   : true,
+    'ter-indent'                      : [true, 2, {
+      'SwitchCase': 2,
+      'VariableDeclarator': 2,
+      'outerIIFEBody': 2,
+      'FunctionDeclaration': {
+        'body': 2,
+      },
+      'FunctionExpression': {
+        'body': 2,
+      },
+    }],
+    'forin'                           : true,
+    'no-arg'                          : true,
+    'no-eval'                         : true,
+    'no-switch-case-fall-through'     : true,
+    'label-position'                  : true,
+    'no-unused-expression'            : true,
+    'no-construct'                    : true,
+    'no-duplicate-variable'           : [true, 'check-parameters'],
+    'no-string-throw'                 : true,
+    'radix'                           : true,
+
+    /**
+     * Style
+     */
+    'brace-style'                     : [
+      true,
+      '1tbs',
+      {
+        'allowSingleLine' : true,
+      },
+    ],
+    'quotemark'                       : [true, 'single', 'jsx-double'],
+    'variable-name'                   : [true, 'check-format', 'allow-pascal-case', 'ban-keywords'],
+    'eofline'                         : true,
+    'no-consecutive-blank-lines'      : [true, 2],
+    'no-trailing-whitespace'          : true,
+    'one-variable-per-declaration'    : true,
+    'semi'                            : [true, 'always'],
+    'comment-format'                  : [true],
+  },
+};
